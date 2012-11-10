a42d8b85cd33b5275b73805783c999392eab71fc
Nate Riffe <inkblot@movealong.org>
2012-11-10 11:10:33 -0600

    Create a resource for controlling a CloudFormation stack

---
+++ lib/puppet/provider/cfn_stack/aws.rb
@@ -0,0 +1,35 @@
+# ex:ts=2 sw=2 tw=72
+
+require 'fog'
+
+include Fog::AWS
+
+Puppet::Type.type(:cfn_stack).provide(:aws) do
+
+	def exists?
+		!cf.describe_stacks.body['Stacks'].index { |stack| stack['StackName'] == @resource[:name] }.nil?
+	end
+
+	def create
+		options = {}
+		if @resource[:template_file]
+			options['TemplateBody'] = IO.read(@resource[:template_file])
+		elsif @resource[:template_url]
+			options['TemplateURL'] = @resource[:template_url]
+		end
+		if @resource[:parameters]
+			options['Parameters'] = @resource[:parameters]
+		end
+		cf.create_stack(@resource[:name], options)
+	end
+
+	def destroy
+		cf.delete_stack(@resource[:name])
+	end
+
+	private
+	def cf
+		CloudFormation.new(:aws_access_key_id => @resource[:aws_access_key_id], :aws_secret_access_key => @resource[:aws_secret_access_key])
+	end
+
+end
---
+++ lib/puppet/type/cfn_stack.rb
@@ -0,0 +1,46 @@
+# ex:ts=2 sw=2 tw=72
+
+Puppet::Type.newtype(:cfn_stack) do
+
+	desc <<-EOT
+		Ensures that a specified AWS CloudFormation Stack is in the desired state.
+	EOT
+
+	ensurable
+
+	newparam(:name, :namevar => true) do
+		desc 'An arbitrary name for the CloudFormation stack.'
+	end
+
+	newparam(:template_file) do
+		desc 'A file resource for a CloudFormation template.'
+	end
+
+	newparam(:template_url) do
+		desc 'The URL of a CloudFormation template.'
+	end
+
+	newparam(:parameters) do
+		desc 'A hash of parameters to use with the template.'
+	end
+
+	newparam(:aws_access_key_id) do
+		desc 'The AWS Access Key Id to use for authentication with AWS.'
+	end
+
+	newparam(:aws_secret_access_key) do
+		desc 'The AWS Secret Key that corresponds to the AWS Access Key Id.'
+	end
+
+	validate do
+		if self[:template_file] and self[:template_url]
+			raise(Puppet::Error, 'Only one of template_file or template_url may be specified')
+		end
+		unless self[:template_file] or self[:template_url]
+			raise(Puppet::Error, 'One of template_file or template_url must be specified.')
+		end
+		unless self[:aws_access_key_id] and self[:aws_secret_access_key]
+			raise(Puppet::Error, 'aws_access_key_id and aws_secret_access_key are required.')
+		end
+	end
+end
