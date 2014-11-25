75344fdfb41b4ab5b0e3974a35d9a427cf17a1eb
Nate Riffe <inkblot@movealong.org>
2014-11-25 08:32:17 -0600

    Add a s3_object_content for fetching an S3 object

---
+++ lib/puppet/parser/functions/s3_object_content.rb
@@ -0,0 +1,19 @@
+# ex: syntax=ruby sw=2 ts=2 si et
+require 'aws/s3'
+
+module Puppet::Parser::Functions
+
+  newfunction(:s3_object_content, :type => :rvalue) do |args|
+    bucket = args[0]
+    key = args[1]
+    region = args[2] || 'us-east-1'
+
+    c = AWS::S3::Client.new(:region => region)
+    r = c.get_object(:bucket_name => bucket, :key => key)
+    if r.successful?
+      r.data[:data]
+    else
+      raise Puppet::Error "Could not read S3 object content"
+    end
+  end
+end
